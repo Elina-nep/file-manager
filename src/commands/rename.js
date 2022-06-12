527adc24c789690256753a01ae77b859627fce33
elina <elina.nep@gmail.com>
2022-06-12 15:46:41 +0300

    fix: rename

--- src/commands/rename.js
+++ src/commands/rename.js
@@ -1,16 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 import { dirname } from 'path'
+import { consolePath } from './consolePath.js';
+import { getAbsPath } from './getAbsPath.js';
 
 export const rename = async(pathToFile, newFilename) => {
 
   const absPathToFile = getAbsPath(pathToFile);
-  const absPathToNewFile = path.normalize(`${String((absPathToFile.split(path.sep)).slice(-1))}/${newFilename}`);
+  const absPathToNewFile = [((absPathToFile.split(path.sep)).slice(0, -1)).join(path.sep), path.sep, newFilename].join('');
+  console.log(absPathToNewFile)
 
   fs.access(absPathToNewFile, (err) => {
-    if (!err) { console.log('Operation failed') } else {
-      fs.rename(absPathToFile, newFilename, err => {
+    if (!err) {
+      console.log('Operation failed')
+      consolePath()
+    } else {
+      fs.rename(absPathToFile, absPathToNewFile, err => {
         if (err) console.log('Operation failed')
+        consolePath()
       });
     }
   })
